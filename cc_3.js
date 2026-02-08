4d7414bdf9a2f22a3326fcded30a487aab65eef9
Asce260901 <ajm55@usf.edu>
2026-02-07 20:50:58 -0500

    Initial commitand setup

---
+++ cc_3.js
@@ -0,0 +1,17 @@
+let customers = [
+   {
+    full_name: "Alejandro Heredia",
+    email: "course1@gmail.com",
+    purchases:["Jacket", "TV","Headphones"]
+   },
+   {
+    full_name: "Josh Simmons",
+    email:"drakeandjosh@gmail.com",
+    purchases: ["Guitar","Drums","Piano"]
+   },
+   {
+    full_name:"Dante Black",
+    email: "newhistory@gmail.com",
+    purchases:["Toy car","Book The name of god","Red Paint"]
+   }
+]
