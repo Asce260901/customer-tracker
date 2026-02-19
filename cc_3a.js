9732b3c6f854f2303f4c4052ae8331f3233780af
Alejandro Martinez <ajm55@usf.edu>
2026-02-19 13:11:42 -0500

    Update cc_3a.js

--- cc_3a.js
+++ cc_3a.js
@@ -1,16 +1,16 @@
 let customers = [
    {
-    full_name: "Alejandro Heredia",
+    fullName: "Alejandro Heredia",
     email: "course1@gmail.com",
     purchases:["Jacket", "TV","Headphones"]
    },
    {
-    full_name: "Josh Simmons",
+    fullName: "Josh Simmons",
     email:"drakeandjosh@gmail.com",
     purchases: ["Guitar","Drums","Piano"]
    },
    {
-    full_name:"Dante Black",
+    fullName:"Dante Black",
     email: "newhistory@gmail.com",
     purchases:["Toy car","Book The name of god","Red Paint"]
    }
